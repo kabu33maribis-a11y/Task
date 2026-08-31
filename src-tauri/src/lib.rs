9492c843a76296c8d2002dd454c39128fe31f400
kabu33maribis-a11y <kabu33maribis@gmail.com>
2026-08-31 12:05:03 +0900

    feat: add window state persistence

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,6 +21,7 @@ pub fn run() {
       Ok(())
     })
     .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_window_state::Builder::default().build())
     .plugin(tauri_plugin_store::Builder::default().build())
     .plugin(tauri_plugin_dialog::init())
     .plugin(
